50e53b0e135a725363fd9983210804b544f26d35
Rufus Pollock <rufus.pollock@okfn.org>
2013-04-16 10:46:03 +0100

    [scripts/process][s]: make keys out of slugs.
    * code to make slug from title (for de case)
    * change key to {jurisdiction}/{slug} and then drop slug

--- scripts/process.py
+++ scripts/process.py
@@ -1,8 +1,8 @@
 import csv
 import sys
-path = sys.argv[1]
+import os
 
-def normalize():
+def normalize(path):
     out = [];
     fo = open(path) 
     reader = csv.DictReader(fo)
@@ -10,16 +10,36 @@ def normalize():
     for row in reader:
         newrow = dict(row)
         slug = row['slug']
-        if slug:
-            pass
-        else:
-            slug = row['title'].strip().replace('.', '')
+        if not slug:
+            slug = row['title']
+            slug = strip_accents(slug)
+            slug = slug.lower()
+            slug = slug.strip().replace('.', '')
+            slug = slug.replace('/', ' ')
+            slug = slug.replace(' ', '-')
+            slug = slug.replace('- ', '-')
+            slug = slug.replace('--', '-')
+        newrow['key'] = row['key'].split('/')[0] + '/' + slug
+        del newrow['slug']
         out.append(newrow) 
     fo.close()
+    fields = [ f for f in fields if f != 'slug' ]
     writer = csv.DictWriter(open(path, 'w'), fields, lineterminator='\n')
     writer.writeheader()
     writer.writerows(out)
 
+import unicodedata
+def strip_accents(s):
+    s = s.decode('utf8')
+    out = ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+    return out.encode('utf8')
+
 if __name__ == '__main__':
-    normalize()
+    if len(sys.argv) >= 2:
+        path = sys.argv[1]
+        normalize(path)
+    else:
+        for c in ['de', 'eu', 'gb']:
+            normalize(os.path.join('data', c + '.csv'))
 
