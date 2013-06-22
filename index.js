a3bfd47eef171fb6b39b2e4cb414e9a78d718ae9
Rowan Crawford <wombleton@gmail.com>
2013-06-22 22:15:04 +1200

    add jurisdction list to  front page

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var express = require('express'),
     routes = require('./routes'),
     http = require('http'),
     path = require('path'),
-    app = express();
+    app = express(),
+    db = require('./lib/db');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -28,6 +29,7 @@ if ('development' === app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/:jurisdiction', routes.jurisdiction);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
+var db = require('../lib/db'),
+    _ = require('underscore');
 
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+    var juris = {};
+
+    db.forEach(function(key, record) {
+        if (record.type === 'body') {
+            var code = record.jurisdiction_code;
+
+            juris[code] = juris[code] || 0;
+
+            juris[code]++;
+        }
+    });
+    res.render('index', {
+        juris: juris
+    });
+};
+
+exports.jurisdiction = function(req, res) {
+    var bodies = [],
+        jurisdiction = req.params.jurisdiction;
+
+    db.forEach(function(key, record) {
+        if (record.jurisdiction_code === jurisdiction) {
+            bodies.push(record);
+        }
+    });
+    res.render('jurisdiction', {
+        bodies: bodies
+    });
 };
