d0e30e852dcf63bb7cfc46609977406e8d550c07
Rowan Crawford <wombleton@gmail.com>
2013-06-23 13:35:41 +1200

    Style bodies page

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(require('less-middleware')({
+    src: __dirname + '/public'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('less-middleware')({
-    src: __dirname + '/public'
-}));
 
 // development only
 if ('development' === app.get('env')) {
