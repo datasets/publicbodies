9e2fb8c21ae3c6effa4c408efb936f2b15599936
Augusto Herrmann <augusto+github@herrmann.tech>
2022-08-30 11:43:08 -0300

    Automate site deployment with Github Actions

---
+++ scripts/deploy/prepare_build.py
@@ -0,0 +1,45 @@
+#/usr/bin/python3
+import os, shutil
+import csv
+import logging
+
+# directories
+DATA_DIR = "../../data"
+WEBSITE_DIR = "../../website"
+BODY_TEMPLATE = \
+"""---
+layout: body
+permalink: {}
+---
+
+"""
+
+logging.getLogger().setLevel(logging.INFO)
+
+# copy data files to website
+data_files = [file for file in os.listdir(DATA_DIR) if file.endswith('.csv')]
+
+for file in data_files:
+    logging.info('Copying data file "%s"...', file)
+    shutil.copy2(
+        os.path.join(DATA_DIR,file),
+        os.path.join(WEBSITE_DIR, "_data"))
+
+# generate files for individual public bodies
+for data_file in data_files:
+    csv_filename = os.path.join(WEBSITE_DIR, "_data", data_file)
+    with open(csv_filename, "r") as csv_file:
+        row_reader = csv.DictReader(csv_file)
+        for row in row_reader:
+            jurisdiction, body_id = row["id"].split("/")
+            jurisdiction_path = os.path.join(WEBSITE_DIR, jurisdiction)
+            try:
+                os.mkdir(jurisdiction_path)
+                logging.info('Created directory "%s"...', jurisdiction)
+            except FileExistsError:
+                pass
+            md_filename = f"{body_id}.md"
+            with open(os.path.join(jurisdiction_path, md_filename),
+                "w") as md_file:
+                md_file.write(
+                    BODY_TEMPLATE.format(f"{jurisdiction}/{body_id}/"))
